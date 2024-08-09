611141d94b4a32432b148a4b7a4a94687c532851
rolandoandre123 <rbardalespriale@gmail.com>
2024-08-09 00:51:29 -0500

    add : import functions from modules

--- 8lec_modules/app.py
+++ 8lec_modules/app.py
@@ -1,7 +1,15 @@
 # module -> It is a single Python file that can be imported into another module.
 
-from usuario_impuesto import guardar, pagar_impuestos
+# You can import functions from module like this.
+# It's bad paractice to import all functions using '*'
+# If you have a module with the same name you have to change its name using 'as'
+from usuario_impuesto import guardar as guardar2, pagar_impuestos
 
-guardar()
-pagar_impuestos()
-# module -> It is a single Python file that can be imported into another module.
+
+def guardar():
+    print("soy app.py")
+
+
+guardar()  # soy app.py
+guardar2()  # guardando
+pagar_impuestos()  # pagando impuestos
