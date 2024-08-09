9de0fe0f74ebba2ea7441d4d2de0e9542c6ad0f9
rolandoandre123 <rbardalespriale@gmail.com>
2024-08-09 00:27:50 -0500

    fix : renaming modules and packagen acording naming conventions

---
+++ 8lec_modules/app.py
@@ -0,0 +1,7 @@
+# module -> It is a single Python file that can be imported into another module.
+
+from usuario_impuesto import guardar, pagar_impuestos
+
+guardar()
+pagar_impuestos()
+# module -> It is a single Python file that can be imported into another module.
